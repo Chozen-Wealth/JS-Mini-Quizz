1f706cfeb5738564dde34a196c432b2e9b282503
chozen <moustafa.eldehni@hotmail.com>
2025-03-11 11:33:11 +0100

    Création de const tableau avec les questions et réponses.

---
+++ public/js/main.js
@@ -0,0 +1,43 @@
+// Données
+const questions = [
+    {
+        question: "Quel est le nom complet du père de Naruto ?",
+        reponse : "Minato Namikaze"
+    },
+    {
+        question: "Quelle est la technique signature de Kakashi Hatake ?",
+        reponse : "Chidori"
+    },
+    {
+        question: "Comment s'appelle le démon renard scellé en Naruto ?",
+        reponse : "Kurama"
+    },
+    {
+        question: "Quel est le nom du village caché de Naruto ?",
+        reponse : "Konoha"
+    },
+    {
+        question: "Qui est le premier Hokage de Konoha ?",
+        reponse : "Hashirama"
+    },
+    {
+        question: "Quel est le dojutsu du clan Uchiha ?",
+        reponse : "Sharingan"
+    },
+    {
+        question: "Comment s'appelle l'organisation criminelle à laquelle appartient Itachi ?",
+        reponse : "Akatsuki"
+    },
+    {
+        question: "Qui est le maître de Jiraiya et Tsunade ?",
+        reponse : "Hiruzen"
+    },
+    {
+        question: "Quel est le nom du frère cadet d'Itachi ?",
+        reponse : "Sasuke"
+    },
+    {
+        question: "Quel est le nom de l'arme utilisée par Sasuke Uchiha pendant sa quête de vengeance ?",
+        reponse : "Katana"
+    },
+]
